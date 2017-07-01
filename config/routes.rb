b17911889135020361840b43bc5cd327fc5783ef
Y.N <y.naka.111.739@gmail.com>
2017-07-01 13:19:45 +0000

    top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'toppages/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'tasks#index'
+  # root to: 'tasks#index'
+  root to: 'toppages#index'
   
   resources :tasks
 end
